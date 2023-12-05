0115aa1a8ff212ea2e619ebd4b10790464dff81f
combateer3 <scottcorbat@yahoo.com>
2023-12-04 21:21:00 -0500

    setup grid display on main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import styles from './page.module.css'
 export default function Root() {
   return (
     <main className={styles['main']}>
-      <p>This is the main element!</p>
+      <p className={styles['desc']}>
+        Hi there! I'm Scott Corbat. I'm currently working as a controls engineer at HTI Cybernetics.
+        I studied Computer Science at Michigan Technological University, and earned my bachelor's degree
+        there. In my free time I enjoy programming, powerlifting, videography, and video games.
+      </p>
     </main>
   )
 }
